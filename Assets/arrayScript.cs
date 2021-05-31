91196b1d3ac6ff54c726e47b4c1a1c584dda5b2c
RKarlhuber <r.karlhuber@outlook.com>
2021-05-31 10:32:12 +0200

    all

---
+++ Assets/arrayScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrayScript : MonoBehaviour
+{
+
+    string[] array1 = new string[4]; 
+    float[] array2 = new float[4]; 
+    int[] array3 = new int[4]; 
+
+    // 1) Durchlaufen ausgeben der Werte
+    // 2) Länge aus Arrays ausgeben als Debug
+    // 3) 2 Werte direkt ändern
+    // 4) GameObjects Images anlegen, Tag vergeben, Zuweisen und Farbe ändern
+
+}
